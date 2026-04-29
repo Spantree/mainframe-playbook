d4870041fc6d534bf785e86256f7ca7c1ab9a2ed
Cedric Hurst <cedric@spantree.net>
2026-04-29 10:52:02 -0500

    feat(docs): register FullWidthImage component for lightbox support
    FullWidthImage is now available in all MDX pages. Usage:
    <FullWidthImage src="..." alt="..." caption="..." />
    Clicking any image opens a lightbox overlay. CSS already in custom.css.

--- docs/src/theme/MDXComponents.tsx
+++ docs/src/theme/MDXComponents.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Carbon from '@site/src/components/Carbon';
+import FullWidthImage from '@site/src/components/FullWidthImage';
 
 export default {
   ...MDXComponents,
   Carbon,
+  FullWidthImage,
 };
